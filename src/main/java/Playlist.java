c9f669c411e0f8c67aa9339fd8ddbb1645dd3aaf
William Nguyen <williamnguyen@Williams-MacBook-Pro.local>
2019-11-01 17:44:14 -0700

    playlist class and authenticate class created

--- src/main/java/Playlist.java
+++ src/main/java/Playlist.java
@@ -1,3 +1,6 @@
+import com.wrapper.spotify.model_objects.specification.Track;
+import com.wrapper.spotify.model_objects.specification.Playlist;
+
 import java.util.ArrayList;
 
 /*
@@ -7,23 +10,49 @@ import java.util.ArrayList;
     • Stores a collection of songs
 */
 public class Playlist {
+    private Authenticate user;
+    private String name;
+    private String playlistID;
+    private ArrayList<Track> collection; // Collection of songs
+    private boolean nameAlreadyExists;
 
-    public Playlist()
-    {
-        collection = new ArrayList<>();
+    public Playlist(String name, Authenticate user) {
+        this.name = name;
+        this.user = user;
+        this.collection = new ArrayList<>();
+        this.nameAlreadyExists = true;
     }
 
-    public void add(Song s) {
-        // Adding unique songs only
+    public String createPlaylist(String userID, Authenticate user) {
+        if(!this.nameAlreadyExists) {
+            try {
+                com.wrapper.spotify.model_objects.specification.Playlist playlist = user.getSpotifyApi()
+                        .createPlaylist(userID, this.name).public_(false).build().execute();
+                System.out.println("Your playlist " + this.name + " has been created.");
+
+                this.playlistID = playlist.getId();
+                return playlist.getId();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                return e.getMessage();
+            }
+        }
+        else {
+            System.out.println("The playlist already exists.");
+            return this.playlistID;
+        }
+    }
+
+    public void add(Track s) {
+        // Adding unique tracks only
         if (!collection.contains(s)) {
             collection.add(s);
         }
     }
 
-    public void remove(Song s) {
-        collection.remove(collection.indexOf(s));
+    public void remove(Track s) {
+        collection.remove(s);
     }
 
-    // Collection of songs
-    private ArrayList<Song> collection;
+
 }
